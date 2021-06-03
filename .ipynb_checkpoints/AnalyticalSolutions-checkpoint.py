c4d918fea5665870ea3852e2b27da380aa823b13
Sergi Andreu <sergiandreu97@gmail.com>
2021-06-03 08:59:27 +0200

    Analytical solutions and comparison

--- .ipynb_checkpoints/AnalyticalSolutions-checkpoint.py
+++ .ipynb_checkpoints/AnalyticalSolutions-checkpoint.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+
 class Cantilever:
     
-    def __init__(self, a, b, QL, ML, x0, case, c=-0.01, E=1, I=1, L=5):
+    def __init__(self, a, b, QL, ML, x0, case, c=-0.01, E=1, I=1, L=5, q=lambda x: x):
         
         self.E = E
         self.I = I
@@ -15,37 +16,33 @@ class Cantilever:
         self.ML = ML
         self.x0 = x0
         
+        self.c = c
+        self.q = q
+        
         self.case = case
         
-        self.c = c
-        
-    def get_q_primitives(self, x, n):
-        
-        c = self.c
-        
+        if self.case == "arbitrary function":
+            
+            import scipy.integrate as integrate
+
+            self.q1 = lambda x: integrate.quad(self.q, 0, x)[0]
+            self.q2 = lambda x: integrate.quad(self.q1, 0, x)[0]
+            self.q3 = lambda x: integrate.quad(self.q2, 0, x)[0]
+            self.q4 = lambda x: integrate.quad(self.q3, 0, x)[0]
+
         if self.case == "constant":
-            if n == 1:
-                return c*x
-            if n == 2:
-                return c*np.power(x,2)/2
-            if n == 3:
-                return c*np.power(x,3)/6
-            if n == 4:
-                return c*np.power(x,4)/24
-            
+            self.q1 = lambda x: c*x
+            self.q2 = lambda x: c*np.power(x,2)/2
+            self.q3 = lambda x: c*np.power(x,3)/6
+            self.q4 = lambda x: c*np.power(x,4)/24
+
         if self.case == "delta":
-            
-            if x <= self.x0:
-                return 0
-            else:
-                if n == 1:
-                    return c
-                if n == 2:
-                    return c*(x-self.x0)
-                if n == 3:
-                    return c*(0.5*(x**2 + self.x0**2) - x*self.x0)  
-                if n == 4:
-                    return c*((1/6)*(x**3 - self.x0**3) - 0.5*(self.x0*self.x0*x - self.x0*x*x))
+            self.q1 = lambda x: 0 if x<x0 else 1
+            self.q2 = lambda x: 0 if x<x0 else c*x
+            self.q3 = lambda x: 0 if x<x0 else c*x*x/2
+            self.q3 = lambda x: 0 if x<x0 else c*x*x*x/6
+
+
         
     def get_coefficients(self):
         
@@ -54,27 +51,18 @@ class Cantilever:
         a1 = self.a
         a2 = self.b
         
-        num1 = self.ML - self.get_q_primitives(self.L, 2)
-        den1 = 2*EI
+        a3 = (self.ML + self.QL*self.L + self.q1(self.L)*self.L - self.q2(self.L))/(2*EI)
 
-        num2 = self.L * ( self.QL - self.get_q_primitives(self.L, 1) )
-        den2 = EI
-        
-        a3 = num1/den1 + num2/den2
-
-        num = self.QL - self.get_q_primitives(self.L, 1)
-        den = 6*EI
-        
-        a4 = num/den
+        a4 = -(self.QL + self.q1(self.L))/(6*EI)
         
         return [a1,a2,a3,a4]
     
     
-    def get_solution(self, x):
+    def get_solution_list(self, x):
         
         coef = self.get_coefficients()
         
-        first_term = lambda x : self.get_q_primitives(x, 4) / (self.E * self.I)
+        first_term = lambda x : self.q4(x) / (self.E * self.I)
         
         polynomial = lambda x : coef[0] + coef[1]*x + coef[2]*x**2 + coef[3]*x**3
         
@@ -83,10 +71,38 @@ class Cantilever:
 
         return first_term(x) + polynomial(x)
     
+    def get_solution(self):
+        
+        coef = self.get_coefficients()
+        
+        primitives_4 = self.q4
+        
+        first_term = lambda x : primitives_4(x) / (self.E * self.I)
+        
+        polynomial = lambda x : coef[0] + coef[1]*x + coef[2]*x**2 + coef[3]*x**3
+        
+        
+        sol = lambda x: first_term(x) + polynomial(x)
+
+        return sol
+    
+    def get_solution_prime(self):
+        
+        coef = self.get_coefficients()
+        
+        first_term = lambda x : self.q3(x) / (self.E * self.I)
+        
+        polynomial = lambda x : coef[1] + 2*coef[2]*x + 3*coef[3]*x**2
+        
+        
+        sol = lambda x: first_term(x) + polynomial(x)
+
+        return sol
+    
     
 class BothEnds:
     
-    def __init__(self, A1, B1, A2, B2, x0, case, c=-0.01, E=1, I=1, L=5):
+    def __init__(self, A1, B1, A2, B2, x0, case, c=-0.01, E=1, I=1, L=5, q= lambda x: x):
         
         self.E = E
         self.I = I
@@ -99,37 +115,33 @@ class BothEnds:
         self.B2 = B2
         self.x0 = x0
         
+        self.c = c
+        self.q = q
+        
+        
         self.case = case
         
-        self.c = c
-        
-    def get_q_primitives(self, x, n):
-        
-        c = self.c
-        
+        if self.case == "arbitrary function":
+            
+            import scipy.integrate as integrate
+
+            self.q1 = lambda x: integrate.quad(self.q, 0, x)[0]
+            self.q2 = lambda x: integrate.quad(self.q1, 0, x)[0]
+            self.q3 = lambda x: integrate.quad(self.q2, 0, x)[0]
+            self.q4 = lambda x: integrate.quad(self.q3, 0, x)[0]
+
         if self.case == "constant":
-            if n == 1:
-                return c*x
-            if n == 2:
-                return c*np.power(x,2)/2
-            if n == 3:
-                return c*np.power(x,3)/6
-            if n == 4:
-                return c*np.power(x,4)/24
-            
+            self.q1 = lambda x: c*x
+            self.q2 = lambda x: c*np.power(x,2)/2
+            self.q3 = lambda x: c*np.power(x,3)/6
+            self.q4 = lambda x: c*np.power(x,4)/24
+
         if self.case == "delta":
+            self.q1 = lambda x: 0 if x<x0 else 1
+            self.q2 = lambda x: 0 if x<x0 else c*x
+            self.q3 = lambda x: 0 if x<x0 else c*x*x/2
+            self.q3 = lambda x: 0 if x<x0 else c*x*x*x/6
             
-            if x <= self.x0:
-                return 0
-            else:
-                if n == 1:
-                    return c
-                if n == 2:
-                    return c*(x-self.x0)
-                if n == 3:
-                    return c*(0.5*(x**2 + self.x0**2) - x*self.x0)  
-                if n == 4:
-                    return c*((1/6)*(x**3 - self.x0**3) - 0.5*(self.x0*self.x0*x - self.x0*x*x))
         
     def get_coefficients(self):
         
@@ -138,21 +150,26 @@ class BothEnds:
         a1 = self.A1
         a2 = self.B1
         
-        term1 = (1/self.L**2)*((self.B2 + self.B1)/2 + ((self.A1 - self.A2)/self.L))
-        term2 = (1/(EI*self.L**2))*( (self.get_q_primitives(self.L, 4)/self.L) - (self.get_q_primitives(self.L, 3)/2) )
         
-        a4 = term1+term2
+        phi1 = self.q4(self.L)/EI
+        phi2 = self.q3(self.L)/EI
         
-        a3 = -(3*self.L/2)*a4 + (self.B2 - self.B1)/(2*self.L) - (self.get_q_primitives(self.L, 3)/(2*self.L*EI))
         
-        return [a1,a2,a3,a4]
+        a4 = 2*(self.A1 - self.A2) + self.L*(self.B1 + self.B2) - self.L*phi2 + 2*phi1
+        a4 = a4 / (self.L ** 3)
+        
+        a3 = (-3/2) * a4 * self.L **3 + (self.L/2)*(self.B2 - self.B1) - self.L*phi2/2
+        
+        a3 = a3/(self.L*self.L)
+        
+        return [a1, a2, a3, a4]
     
     
-    def get_solution(self, x):
+    def get_solution_list(self, x):
         
         coef = self.get_coefficients()
         
-        first_term = lambda x : self.get_q_primitives(x, 4) / (self.E * self.I)
+        first_term = lambda x : self.q4(x) / (self.E * self.I)
         
         polynomial = lambda x : coef[0] + coef[1]*x + coef[2]*x**2 + coef[3]*x**3
         
@@ -160,3 +177,28 @@ class BothEnds:
         polynomial = np.vectorize(polynomial)
 
         return first_term(x) + polynomial(x)
+    
+    def get_solution(self):
+        
+        coef = self.get_coefficients()
+        
+        first_term = lambda x : self.q4(x) / (self.E * self.I)
+        
+        polynomial = lambda x : coef[0] + coef[1]*x + coef[2]*x**2 + coef[3]*x**3
+        
+        sol = lambda x: first_term(x) + polynomial(x)
+        
+        return sol
+        
+        
+    def get_solution_prime(self):
+        
+        coef = self.get_coefficients()
+        
+        first_term = lambda x : self.q3(x) / (self.E * self.I)
+        
+        polynomial = lambda x : coef[1] + 2*coef[2]*x + 3*coef[3]*x**2
+        
+        sol = lambda x: first_term(x) + polynomial(x)
+
+        return sol
